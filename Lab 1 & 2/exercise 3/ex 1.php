de5b1b155f5eefdf2db7fed98d3202117e3e8d45
MR <mredosz4@gmail.com>
2023-03-11 19:06:40 +0100

    text

--- Lab 1 & 2/exercise 3/ex 1.php
+++ Lab 1 & 2/exercise 3/ex 1.php
@@ -3,3 +3,53 @@
  * Napisz funkcję, zwracającą maksymalny element tablicy losowych liczb
  * (bez używania gotowych funkcji PHP) w 4 wersjach: for, while, do while, foreach.
  */
+for ($i = 0; $i < 100; $i++) {
+    $tab[$i] = rand(1,100);
+}
+
+function maxFor($tab){
+    $max = 0;
+    for ($i = 0; $i < count($tab); $i++) {
+        if ($tab[$i]>$max){
+            $max = $tab[$i];
+        }
+    }
+    return "Max value in for function is: ".$max.'<br>';
+}
+function maxWhile($tab){
+    $max = 0;
+    $i = 0;
+    while ($i < count($tab)) {
+
+        if ($tab[$i]>$max){
+            $max = $tab[$i];
+        }
+        $i++;
+    }
+    return "Max value in while function is: ".$max.'<br>';
+}
+function maxDowhile($tab){
+    $max = 0;
+    $i = 0;
+    do {
+        if ($tab[$i]>$max){
+            $max = $tab[$i];
+        }
+        $i++;
+    }while ($i < count($tab));
+    return "Max value in do while function is: ".$max.'<br>';
+}
+function maxForeach($tab){
+    $max = 0;
+    foreach ($tab as $item) {
+        if ($item > $max) {
+            $max = $item;
+        }
+    }
+    return "Max value in foreach function is: ".$max.'<br>';
+}
+
+echo maxFor($tab);
+echo maxWhile($tab);
+echo maxDowhile($tab);
+echo maxForeach($tab);
--- Lab 1 & 2/exercise 3/ex 4.php
+++ Lab 1 & 2/exercise 3/ex 4.php
@@ -5,3 +5,24 @@
  * potrzebne dowykonania obliczeń. Spróbuj tak zmodyfikować program, by było
  * potrzeba jak najmniej iteracji (przy zachowaniu prawidłowego działania).
  */
+
+function primeNumber($number, &$repeat){
+
+    if($number==1){
+        return false;
+    }
+    for ($i = 2, $repeat = 1; $i <=$number/2; $i++,$repeat++){
+        if ($number%$i==0){
+            return false;
+        }
+    }
+return true;
+}
+$tabTest = [100,11,666,911,1234567];
+
+foreach ($tabTest as $item){
+    $repeat = 0;
+    if (primeNumber($item,$repeat)){
+        echo $item.' yes, repeat '.$repeat.'<br>';
+    }else  echo $item.' no, repeat '.$repeat.'<br>';
+}
